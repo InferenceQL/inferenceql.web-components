d3dcabc42efb46a1e30fe4b713955acffb6a489f
Zane Shelby <git@zane.io>
2024-01-19 20:40:59 -0500

    feat(vega-lite): Add form support

--- vega-lite.mjs
+++ vega-lite.mjs
@@ -10,6 +10,21 @@ export class VegaLite extends LitElement {
     opt: { type: Object, converter: (s) => JSON5.parse(s) },
   };
 
+  static formAssociated() {
+    return true;
+  }
+
+  constructor() {
+    super();
+    this.internals = this.attachInternals();
+    this.spec = this.spec || JSON5.parse(this.textContent);
+  }
+
+  firstUpdated(...args) {
+    super.firstUpdated(...args);
+    this.internals.setFormValue(this.spec);
+  }
+
   disconnectedCallback() {
     super.disconnectedCallback();
     if (this.#finalize) {
@@ -17,11 +32,36 @@ export class VegaLite extends LitElement {
     }
   }
 
+  _hasInlineData() {
+    return (
+      this.spec.hasOwnProperty('data')
+      && this.spec.data.hasOwnProperty('values')
+      && Array.isArray(this.spec.data.values)
+    );
+  }
+
+  _hasDataFromURL() {
+    return (
+      this.spec.hasOwnProperty('data')
+      && Array.isArray(this.spec.data)
+    );
+  }
+
+  _hasNamedDataSources() {
+    return (
+      this.spec.hasOwnProperty('data')
+      && this.spec.data.hasOwnProperty('name')
+      && typeof this.spec.data.name === 'string'
+    );
+  }
+
   render() {
-    const spec = this.spec || JSON5.parse(this.textContent);
-    const element = document.createElement('div');
-    vegaEmbed(element, spec, this.opt).then((result) => this.#finalize = result.finalize);
-    return element;
+    const data = this.spec.data
+    if (this._hasInlineData() || this._hasDataFromURL() || this._hasNamedDataSources()) {
+      const element = document.createElement('div');
+      vegaEmbed(element, this.spec, this.opt).then((result) => this.#finalize = result.finalize);
+      return element;
+    }
   }
 }
 
